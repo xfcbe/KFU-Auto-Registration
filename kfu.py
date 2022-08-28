eb6cfac9fca43663b492c5d748761f7165052e2c
TrueTurki <73060562+xfcbe@users.noreply.github.com>
2022-08-28 20:30:07 +0300

    Update kfu.py

--- kfu.py
+++ kfu.py
@@ -1,19 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from colorama import Fore
-
+from selenium.webdriver.support.ui import Select
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 i = 0
 crn = []
 corses = int(input('how many courses do you have: '))
 for x in range(corses):
-	crn_codes= input('Enter CRN code for the course number '+ str(x+1)+': ')
-	crn.append(crn_codes)
+	crn_codes= input('Enter the crn code for the course number '+ str(x+1)+': ')
+	if crn_codes=="":
+		pass
+	else:
+		crn.append(crn_codes)
 
-username= input(Fore.RED+'Enter your student ID ')
+username= input(Fore.RED+'Enter your student id ')
+
 password= input(Fore.RED+'Enter your password ')
-check = input(Fore.GREEN+"do you have a verify key? (y\n) ")
+
+check = input(Fore.GREEN+"do you have a veryfi key? (y\n) ")
+
 if (check == "y"):
-	verify = input(Fore.GREEN+'Enter the verification code: ')
+	verify = input(Fore.GREEN+'Enter the verify code: ')
 	i=i+1
 else:
 	pass
@@ -24,13 +34,14 @@ if __name__ == '__main__':
 	options.add_argument("--start-maximized")
 	options.add_argument('--log-level=3')
 
-	# Provide the path of chromedriver present on your system.
+							# Provide the path of chromedriver present on your system.
 	driver = webdriver.Chrome(executable_path="chromedriver",
 							chrome_options=options)
 	driver.set_window_size(1680,1050)
 
-		# Send a get request to the url
+							# Send a get request to the url
 	driver.get(url)
+											#login to kfu banar
 	inputElement1 = driver.find_element_by_id("UserID")
 	inputElement = driver.find_element_by_name("PIN")
 	inputElement1.send_keys(username)
@@ -39,28 +50,52 @@ if __name__ == '__main__':
 	if (i==1):
 		pass
 	else:
-		verify = input("Enter the code you have received on your phone: ")
+		verify = input("Enter the code you receve in your phone: ")
 	inputElement2 = driver.find_element_by_name("VerC")
 	inputElement2.send_keys(verify)
 	inputElement2.send_keys(Keys.ENTER)
-
+	time.sleep(1)
 	button3 = driver.find_element_by_id("bmenu--P_StuMainMnu___UID1").click()
-	button4 = driver.find_element_by_id("bmenu--P_RegMnu___UID0").click()
-	button5 = driver.find_element_by_id("contentItem11").click()
-	
+	try:
+
+		element = WebDriverWait(driver, 10).until(
+		EC.element_to_be_clickable((By.ID, "bmenu--P_RegMnu___UID0")))
+		element.click()
+		element1 = WebDriverWait(driver, 10).until(
+		EC.element_to_be_clickable((By.ID, "contentItem11")))
+		element1.click()
+	except:
+		driver.quit()
+	select = Select(driver.find_element_by_id('term_id'))
+	select.select_by_index(1)
+	driver.find_element_by_id("id____UID6").click()
+												#coursees registration
 	for crn_number in crn:
-		inputElement3 = driver.find_element_by_id("")
+		inputElement3 = driver.find_element_by_id("crn_id1")
 		inputElement3.send_keys(crn_number)
 		inputElement3.send_keys(Keys.ENTER)
 		print(crn_number+" -----> "+Fore.GREEN+"Done")
+										
+											#confirm the registrations
+	for d in range(corses):
+		select = Select(driver.find_element_by_id('action_id'+ str(d+1)))		
+		select.select_by_value("")
 		
-	check2 = input("do you want to check your regstration status? y/n")
-	
+											#take a screenshot of a weekly schedule 
+	check2 = input("do you want to check your weekly Schedule? Y/N ")
+
 	if check2=='y':
-		button6 = driver.find_element_by_id("").click()
-		button7 = driver.find_element_by_id("").click()
-		time.sleep(3)
-		driver.save_screenshot('New-Schedule.png')
-		driver.quit()
+		driver.back()
+		driver.back()
+		try:
+			element = WebDriverWait(driver, 5).until(
+			EC.element_to_be_clickable((By.ID, "contentItem110")))
+			element.click()
+			time.sleep(5)
+			driver.save_screenshot('New-Schedule.png')
+			print("Screenshot has been taken ")
+			driver.quit()
+		except:
+			driver.quit()
 	else:
 		driver.quit()
